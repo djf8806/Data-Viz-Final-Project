c5cb1d67320e201af1dbff0d5348abf68fe4e977
djf8806 <djf8806@nyu.edu>
2025-11-30 12:01:44 -0500

    Spatialized LODES data

--- script.r
+++ script.r
@@ -1,2 +1,36 @@
 library(tidyverse)
 library(janitor)
+library(lehdr)
+library(tidycensus)
+
+# Download LODES data (origin-destination) for New York
+
+lodes <- grab_lodes(
+  state = "ny",
+  year = 2022,
+  version = "LODES8",
+  lodes_type = "od",
+  job_type = "JT00",
+  segment = "S000",
+  state_part = "main",
+  agg_geo = "tract"
+) %>% 
+  clean_names()
+
+# Download census tract geometry, population data, and household income data
+nyc_census <- get_acs(
+  state = "ny",
+  geography = "tract",
+  year = 2022,
+  variables = c(population = "B01003_001",
+                househ_income = "B19061_001"),
+  output = "wide",
+  geometry = TRUE
+) %>% 
+  clean_names()
+
+# Merge census data to LODES data by home tracts, filter for NYC
+nyc_census_lodes <- lodes %>%  
+  left_join(nyc_census,
+            by = c("h_tract" = "geoid"))%>% 
+  filter(str_detect(name, "Bronx County|Queens County|Kings County|New York County|Richmond County"))
