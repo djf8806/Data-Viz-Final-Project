3fc2a2ac33847fda81c3bd852ae06d5d37d58d25
djf8806 <djf8806@nyu.edu>
2025-12-03 14:43:58 -0500

    LODES data organized by tract

--- script.r
+++ script.r
@@ -4,7 +4,6 @@ library(lehdr)
 library(tidycensus)
 
 # Download LODES data (origin-destination) for New York
-
 lodes <- grab_lodes(
   state = "ny",
   year = 2022,
@@ -16,6 +15,22 @@ lodes <- grab_lodes(
   agg_geo = "tract"
 ) %>% 
   clean_names()
+
+# Summarize by home tract, sum number of total workers in each tract
+workers_home <- lodes %>% 
+  group_by(h_tract) %>% 
+  summarize(residents = sum(s000))
+
+# Summarize by work tract, sum total number of workers in each tract
+workers_work <-  lodes %>% 
+  group_by(w_tract) %>% 
+  summarize(workers = sum(s000))
+
+# Join residents and workers
+lodes_tracts <- workers_home %>% 
+  full_join(workers_work,
+            by = c("h_tract" = "w_tract")) %>% 
+  rename(tract = h_tract)
 
 # Download census tract geometry, population data, and household income data
 nyc_census <- get_acs(
@@ -29,8 +44,11 @@ nyc_census <- get_acs(
 ) %>% 
   clean_names()
 
-# Merge census data to LODES data by home tracts, filter for NYC
-nyc_census_lodes <- lodes %>%  
+# Merge census data to LODES data by tract, filter for NYC
+census_lodes <- lodes_tracts %>%  
   left_join(nyc_census,
-            by = c("h_tract" = "geoid"))%>% 
+            by = c("tract" = "geoid"))%>% 
   filter(str_detect(name, "Bronx County|Queens County|Kings County|New York County|Richmond County"))
+
+# Save as SHP
+st_write(census_lodes
